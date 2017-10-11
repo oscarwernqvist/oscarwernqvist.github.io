23a0c800da6fc09a6c4047d03883f2c8794dc1f2
oscarwernqvist <32632202+oscarwernqvist@users.noreply.github.com>
2017-10-11 18:42:05 +0200

    Galna ljud
    I had to do javascript also

--- js/javayo.js
+++ js/javayo.js
@@ -1,26 +1,4 @@
-var list = document.getElementById("something"),
-button = document.getElementById("shuffle");
-function shuffle(items)
-{
-    var cached = items.slice(0), temp, i = cached.length, rand;
-    while(--i)
-    {
-        rand = Math.floor(i * Math.random());
-        temp = cached[rand];
-        cached[rand] = cached[i];
-        cached[i] = temp;
-    }
-    return cached;
-}
-function shuffleNodes()
-{
-    var nodes = list.children, i = 0;
-    nodes = Array.prototype.slice.call(nodes);
-    nodes = shuffle(nodes);
-    while(i < nodes.length)
-    {
-        list.appendChild(nodes[i]);
-        ++i;
-    }
-}
-button.onclick = shuffleNodes;
+var snd = new Audio("../muziko/evilLaugh.mp3");
+snd.play();
+snd.currentTime = 0; //så den spelar skrattet en gång först
+
