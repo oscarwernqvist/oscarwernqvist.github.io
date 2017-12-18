29a70487ac1e697650eaa7fef59a2dbf58de5f64
oscarwernqvist <32632202+oscarwernqvist@users.noreply.github.com>
2017-12-18 11:22:41 +0100

    Randomizing Bilder
    Massa jens så kör inte på 4G från och med nu

--- js/javayo.js
+++ js/javayo.js
@@ -25,3 +25,15 @@ window.type = function type(text, i) {
 		setTimeout(type, Math.floor(Math.random() * 80), new_text, 0);
     }
 }
+
+window.onload = choosePic;
+
+//Alla Jens
+var myPix = new Array("img/jens_bond.png","img/jens_mario.png","img/jens_rick.png","img/jens_wizard.png","img/jens_cowboy.png","img/jens_bandidos.png","img/jens_homer2.png","img/jens_vampyr.png","img/jens_BYGGER.png","img/jens_gentelman.png","img/jens_bard.png","img/jens_ash.png","img/jens_skolavslutning.png","img/jens_witch.png","img/jens_mort2.png","img/jens_luigi.png","img/jens_jul.png","img/jens_s.png","img/jens_Thinker_knom.png","img/jens_barb.png");
+
+function choosePic() {
+     var randomNum = Math.floor(Math.random() * myPix.length);
+		 document.getElementById("myPicture").src = myPix[randomNum];
+		 var randomNum = Math.floor(Math.random() * myPix.length);
+		 document.getElementById("myPicture1").src = myPix[randomNum];
+}
