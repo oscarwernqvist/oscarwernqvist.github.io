2548bb0ef4447a27e7305efbc9b39d1d6692eddf
oscarwernqvist <32632202+oscarwernqvist@users.noreply.github.com>
2017-12-18 11:36:07 +0100

    Snygg kod
    fancy fancy

--- js/javayo.js
+++ js/javayo.js
@@ -29,7 +29,11 @@ window.type = function type(text, i) {
 window.onload = choosePic;
 
 //Alla Jens
-var myPix = new Array("img/jens_bond.png","img/jens_mario.png","img/jens_rick.png","img/jens_wizard.png","img/jens_cowboy.png","img/jens_bandidos.png","img/jens_homer2.png","img/jens_vampyr.png","img/jens_BYGGER.png","img/jens_gentelman.png","img/jens_bard.png","img/jens_ash.png","img/jens_skolavslutning.png","img/jens_witch.png","img/jens_mort2.png","img/jens_luigi.png","img/jens_jul.png","img/jens_s.png","img/jens_Thinker_knom.png","img/jens_barb.png");
+var myPix = new Array("img/jens_bond.png","img/jens_mario.png","img/jens_rick.png","img/jens_wizard.png",
+"img/jens_cowboy.png","img/jens_bandidos.png","img/jens_homer2.png","img/jens_vampyr.png","img/jens_BYGGER.png",
+"img/jens_gentelman.png","img/jens_bard.png","img/jens_ash.png","img/jens_skolavslutning.png",
+"img/jens_witch.png","img/jens_mort2.png","img/jens_luigi.png","img/jens_jul.png","img/jens_s.png",
+"img/jens_Thinker_knom.png","img/jens_barb.png");
 
 function choosePic() {
      var randomNum = Math.floor(Math.random() * myPix.length);
