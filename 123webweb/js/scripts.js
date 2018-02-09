214a7753d7fbf644fff2042248d727b64ec30435
oscarwernqvist <32632202+oscarwernqvist@users.noreply.github.com>
2018-02-09 13:12:55 +0100

    Pertan
    Here we gooo

--- 123webweb/js/scripts.js
+++ 123webweb/js/scripts.js
@@ -1,23 +1,30 @@
 $(document).ready(() => {
+    
 
-
+    /* ALLT PÅ DE ANDRA UNDERSIDORNA SKA GÖMMAS HÄR */
+    $('.minavanner').hide()
     
     $('#n1').on('click', () => {
-        $('#n1').attr('class', 'active')
-        $('#n2').attr('class', 'inactive')
-        $('#n3').attr('class', 'inactive')
+        $('#n1').attr('class', 'active');
+        $('#n2').attr('class', 'inactive');
+        $('#n3').attr('class', 'inactive');
+       
     });
 
     $('#n2').on('click', () => {
-        $('#n2').attr('class', 'active')
-        $('#n1').attr('class', 'inactive')
-        $('#n3').attr('class', 'inactive')
+        $('#n2').attr('class', 'active');
+        $('#n1').attr('class', 'inactive');
+        $('#n3').attr('class', 'inactive');
+        $('.minagym').show();
+        $('.minavanner').hide()
     });
 
     $('#n3').on('click', () => {
-        $('#n3').attr('class', 'active')
-        $('#n2').attr('class', 'inactive')
-        $('#n1').attr('class', 'inactive')
+        $('#n3').attr('class', 'active');
+        $('#n2').attr('class', 'inactive');
+        $('#n1').attr('class', 'inactive');
+        $('.minagym').hide();
+        $('.minavanner').show()
     });
 
 
