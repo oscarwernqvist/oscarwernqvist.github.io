0d11ff9af27f8867299ff0ca0d29c89bbf60215a
oscarwernqvist <32632202+oscarwernqvist@users.noreply.github.com>
2018-04-20 13:23:07 +0200

    power
    power

--- slutprojekt/js/js.js
+++ slutprojekt/js/js.js
@@ -1,27 +1,34 @@
 $(document).ready(() => {
 
 
-    $('.popdown').hide()
+    $('.popdown, aside').hide()
 
 
-   $('#butiker').on('click', () => {
-    
-    $('#kt_pd, #nh_pd').hide();
-    $('#bt_pd').slideToggle(100);
-    
+    $('#butiker').on('click', () => {
+
+        $('#kt_pd, #nh_pd').hide();
+        $('#bt_pd').slideToggle(150);
+
     });
 
 
+    $('.hamburger').on('click', () => {
+
+        $('#aside').slideToggle(150);
+        $('.hamburger').toggleClass("is-active");
+
+    });
+
     $('#kategorier').on('click', () => {
         $('#bt_pd, #nh_pd').hide();
-        $('#kt_pd').slideToggle(100);
-        
+        $('#kt_pd').slideToggle(150);
+
     });
 
     $('#nyheter').on('click', () => {
         $('#kt_pd, #bt_pd').hide();
-        $('#nh_pd').slideToggle(100);
-        
+        $('#nh_pd').slideToggle(150);
+
     });
 
 
