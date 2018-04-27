d2c1562a2c2cb39cf33868ed437d95d9ff5891c6
oscarwernqvist <32632202+oscarwernqvist@users.noreply.github.com>
2018-04-27 14:16:54 +0200

    Herrrrrre gud
    Hela lektionen knas

---
+++ slutprojekt/public/js/js.js
@@ -0,0 +1,35 @@
+$(document).ready(() => {
+
+
+    $('.popdown, aside').hide()
+
+
+    $('#butiker').on('click', () => {
+
+        $('#kt_pd, #nh_pd').hide();
+        $('#bt_pd').slideToggle(150);
+
+    });
+
+
+    $('.hamburger').on('click', () => {
+
+        $('#aside').slideToggle(300);
+        $('.hamburger').toggleClass("is-active");
+
+    });
+
+    $('#kategorier').on('click', () => {
+        $('#bt_pd, #nh_pd').hide();
+        $('#kt_pd').slideToggle(150);
+
+    });
+
+    $('#nyheter').on('click', () => {
+        $('#kt_pd, #bt_pd').hide();
+        $('#nh_pd').slideToggle(150);
+
+    });
+
+
+});
