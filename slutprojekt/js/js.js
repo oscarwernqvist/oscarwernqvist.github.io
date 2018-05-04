c8d78f8d90e07f584edf6ea5d65806b4154fbaab
oscarwernqvist <32632202+oscarwernqvist@users.noreply.github.com>
2018-05-04 13:12:43 +0200

    iajsoidjaio
    pojaspodapsodk

--- slutprojekt/js/js.js
+++ slutprojekt/js/js.js
@@ -33,3 +33,20 @@ $(document).ready(() => {
 
 
 });
+
+function MVsearchFunction() {
+    var input, filter, ul, li, h3, i;
+    input = document.getElementById('MVsearchBar');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("allul");
+    li = ul.getElementsByTagName('li');
+
+    for (i = 0; i < li.length; i++) {
+        h3 = li[i].getElementsByTagName("h3")[0];
+        if (h3.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
